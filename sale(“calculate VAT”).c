575800088ee8a11d6c6c0815d47dc10c17dce297
Thanwat <s6804062612340@email.kmutnb.ac.th>
2025-12-09 15:23:57 +0700

    sale(“calculate VAT”).c

--- sale(“calculate VAT”).c
+++ sale(“calculate VAT”).c
@@ -1,1 +1,10 @@
+#include<stdio.h>
 
+int main(){
+    float n1,n2,n3;
+    scanf("%f%f%f",&n1,&n2,&n3);
+    float d1=n1*0.95,d2=(n1+n2)*0.85,d3=(n1+n2+n3)*0.70;
+    printf("%4.2f\n",d1*1.07);
+    printf("%4.2f\n",d2*1.07);
+    printf("%4.2f",d3*1.07);
+}
