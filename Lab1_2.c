1031cd4588b2042118252dd3753557632d12eb4e
Thanwat <s6804062612340@email.kmutnb.ac.th>
2025-12-09 14:25:59 +0700

    Lab1_2.c

--- Lab1_2.c
+++ Lab1_2.c
@@ -2,13 +2,13 @@
 
 int main(){
     float price1,price2,price3;
-    printf("ราคาสินค้าชิ้นที่ 1 :");
+    printf("price 1 :");
     scanf("%f",&price1);
-    printf("%s%4.2f\n","รับส่วนลด 5% :",(price1)*0.95);
-    printf("ราคาสินค้าชิ้นที่ 2 :");
+    printf("%s%4.2f\n","discount 5% :",(price1)*0.95);
+    printf("price 2 :");
     scanf("%f",&price2);
-    printf("%s%4.2f\n","รับส่วนลด 15% :",(price1+price2)*0.85);
-    printf("ราคาสินค้าชิ้นที่ 3 :");
+    printf("%s%4.2f\n","discount 15% :",(price1+price2)*0.85);
+    printf("price 3 :");
     scanf("%f",&price3);
-    printf("%s%4.2f","รับส่วนลด 30% :",(price1+price2+price3)*0.70);
+    printf("%s%4.2f","discount 30% :",(price1+price2+price3)*0.70);
 }
